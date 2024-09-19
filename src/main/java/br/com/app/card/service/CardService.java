3d753f5f6d192f618e5c64cfa917f16c7446a815
yurifws <yurifws@gmail.com>
2024-09-19 00:22:38 -0300

    feat: implementacao do service

--- src/main/java/br/com/app/card/service/CardService.java
+++ src/main/java/br/com/app/card/service/CardService.java
@@ -1,20 +1,34 @@
 package br.com.app.card.service;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import br.com.app.card.entity.CardEntity;
-import br.com.app.card.exception.CardNotFoundException;
 import br.com.app.card.mapper.CardMapper;
 import br.com.app.card.model.CardRequestModel;
 import br.com.app.card.model.CardResponseModel;
+import br.com.app.card.model.ProductResponseModel;
 import br.com.app.card.repository.CardRepository;
 import lombok.RequiredArgsConstructor;
 
-public class CardService {
+@Service
+@RequiredArgsConstructor
+public class CardService implements ICardService {
+	
+	private final ProductService productService;
+	private final CardRepository cardRepository;
+
+	@Transactional
+	@Override
+	public CardResponseModel insert(CardRequestModel cardRequestModel) {
+		
+		validateIfFindProductById(cardRequestModel);
+		
+		return CardMapper.INSTANCE.toResponseModel(
+				cardRepository.save(CardMapper.INSTANCE.toEntity(cardRequestModel)));
+	}
+
+	private ProductResponseModel validateIfFindProductById(CardRequestModel cardRequestModel) {
+		return productService.getProductById(cardRequestModel.getProductId());
+	}
 
 }
--- src/main/java/br/com/app/card/service/ICardService.java
+++ src/main/java/br/com/app/card/service/ICardService.java
@@ -1,10 +1,10 @@
 package br.com.app.card.service;
 
-import java.util.List;
-
 import br.com.app.card.model.CardRequestModel;
 import br.com.app.card.model.CardResponseModel;
 
 public interface ICardService {
+
+	CardResponseModel insert(CardRequestModel cardRequestModel);
 }
  
