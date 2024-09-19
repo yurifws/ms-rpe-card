f7a2b961c0fdd2239d1008705a28e21245f1bdb0
yurifws <yurifws@gmail.com>
2024-09-19 02:10:37 -0300

    feat: config sqs

---
+++ src/main/java/br/com/app/card/configuration/SqsConfig.java
@@ -0,0 +1,30 @@
+package br.com.app.card.configuration;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+@Configuration
+@EnableSqs
+public class SqsConfig {
+
+	@Value("${cloud.aws.sqs.endpoint}")
+	private String serviceEndpoint;
+
+	@Value("${cloud.aws.region.static}")
+	private String signingRegion;
+
+    @Bean
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+        		.endpointOverride(URI.create(serviceEndpoint))
+        		.region(Region.US_EAST_1)
+        		.build();
+    }
+}
