594c9e1eaaa58398ff16e96731776458dc2869b3
yurifws <yurifws@gmail.com>
2024-09-20 17:21:48 -0300

    feat: inclusao de junit

--- src/test/java/br/com/app/card/entrypoint/controller/CardControllerTest.java
+++ src/test/java/br/com/app/card/entrypoint/controller/CardControllerTest.java
@@ -7,6 +7,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -34,25 +36,25 @@ class CardControllerTest {
 		Long clientId = 1234L;
 		CardResponseModel expected = CardResponseModelTestData.getCardResponseModel();
 		
-		when(cardService.findByClientId(clientId)).thenReturn(expected);
+		when(cardService.findByClientId(clientId)).thenReturn(List.of(expected));
 		
 		mockMvc.perform(MockMvcRequestBuilders.get(PATH_CARDS)
 				.param("clientId", String.valueOf(clientId))
 				.contentType(MediaType.APPLICATION_JSON_VALUE))
 		.andDo(print())
 		.andExpect(status().isOk())
-		.andExpect(jsonPath("$.id").value(expected.getId()))
-		.andExpect(jsonPath("$.number").value(expected.getNumber()))
-		.andExpect(jsonPath("$.password").value(expected.getPassword()))
-		.andExpect(jsonPath("$.status").value(expected.getStatus().name()))
-		.andExpect(jsonPath("$.holderName").value(expected.getHolderName()))
-		.andExpect(jsonPath("$.dateCreated").value(expected.getDateCreated().toString()))
-		.andExpect(jsonPath("$.dateUpdated").value(expected.getDateUpdated().toString()))
-		.andExpect(jsonPath("$.product.id").value(expected.getProduct().getId()))
-		.andExpect(jsonPath("$.product.description").value(expected.getProduct().getDescription()))
-		.andExpect(jsonPath("$.product.status").value(expected.getProduct().getStatus().name()))
-		.andExpect(jsonPath("$.product.dateCreated").value(expected.getProduct().getDateCreated().toString()))
-		.andExpect(jsonPath("$.product.dateUpdated").value(expected.getProduct().getDateUpdated().toString()));
+		.andExpect(jsonPath("$[0].id").value(expected.getId()))
+		.andExpect(jsonPath("$[0].number").value(expected.getNumber()))
+		.andExpect(jsonPath("$[0].password").value(expected.getPassword()))
+		.andExpect(jsonPath("$[0].status").value(expected.getStatus().name()))
+		.andExpect(jsonPath("$[0].holderName").value(expected.getHolderName()))
+		.andExpect(jsonPath("$[0].dateCreated").value(expected.getDateCreated().toString()))
+		.andExpect(jsonPath("$[0].dateUpdated").value(expected.getDateUpdated().toString()))
+		.andExpect(jsonPath("$[0].product.id").value(expected.getProduct().getId()))
+		.andExpect(jsonPath("$[0].product.description").value(expected.getProduct().getDescription()))
+		.andExpect(jsonPath("$[0].product.status").value(expected.getProduct().getStatus().name()))
+		.andExpect(jsonPath("$[0].product.dateCreated").value(expected.getProduct().getDateCreated().toString()))
+		.andExpect(jsonPath("$[0].product.dateUpdated").value(expected.getProduct().getDateUpdated().toString()));
 		
 		verify(cardService).findByClientId(clientId);
 	}
